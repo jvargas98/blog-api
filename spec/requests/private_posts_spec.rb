911b9a7a22a7a399acf00d0a96485c278768fc22
Jorge Vargas <jvargas18@ucol.mx>
2020-04-28 23:22:34 -0500

    Add tests for create and update posts Add tests for create and update posts and update the others tests

--- spec/requests/private_posts_spec.rb
+++ spec/requests/private_posts_spec.rb
@@ -8,6 +8,8 @@ RSpec.describe "Posts with authentication", type: :request do
   let!(:other_user_post_draft) { create(:post, user_id: other_user.id, published: false) }
   let!(:auth_headers) { { "Authorization" => "Bearer #{user.auth_token}" } }
   let!(:other_auth_headers) { { "Authorization" => "Bearer #{other_user.auth_token}" } }
+  let!(:create_params) { { "post" => { "title" => "title", "content" => "content", "published" => true } } }
+  let!(:update_params) { { "post" => { "title" => "title", "content" => "content", "published" => true } } }
 
   describe "GET /posts/{id}" do
     context "with valid auth" do
@@ -33,6 +35,7 @@ RSpec.describe "Posts with authentication", type: :request do
             subject { payload }
             it { is_expected.to include(:error) }
           end
+
           context "response" do
             subject { response }
             it { is_expected.to have_http_status(:not_found) }
@@ -46,14 +49,71 @@ RSpec.describe "Posts with authentication", type: :request do
   end
 
   describe "POST /posts" do
+    context "with valid auth" do
+      before { post "/posts", params: create_params, headers: auth_headers }
+
+      context "payload" do
+        subject { payload }
+        it { is_expected.to include(:id, :title, :content, :published, :author) }
+      end
+
+      context "response" do
+        subject { response }
+        it { is_expected.to have_http_status(:created) }
+      end
+    end
+
+    context "without auth" do
+      before { post "/posts", params: create_params }
+
+      context "payload" do
+        subject { payload }
+        it { is_expected.to include(:error) }
+      end
+
+      context "response" do
+        subject { response }
+        it { is_expected.to have_http_status(:unauthorized) }
+      end
+    end
   end
 
   describe "PUT /posts/{id}" do
+    context "with valid auth" do
+      context "when updating user's post" do
+        before { put "/posts/#{user_post.id}", params: update_params, headers: auth_headers }
+
+        context "payload" do
+          subject { payload }
+          it { is_expected.to include(:id, :title, :content, :published, :author) }
+          it { expect(payload[:id]).to eq(user_post.id) }
+        end
+
+        context "response" do
+          subject { response }
+          it { is_expected.to have_http_status(:ok) }
+        end
+      end
+
+      context "when updating other user's post" do
+        before { put "/posts/#{other_user_post.id}", params: update_params, headers: auth_headers }
+
+        context "payload" do
+          subject { payload }
+          it { is_expected.to include(:error) }
+        end
+
+        context "response" do
+          subject { response }
+          it { is_expected.to have_http_status(:not_found) }
+        end
+      end
+    end
   end
 
   private
 
   def payload
-    payload = JSON.parse(response.body).with_indifferent_access
+    JSON.parse(response.body).with_indifferent_access
   end
 end
